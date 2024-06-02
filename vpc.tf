da283eb028dcc40f2b190568305cb16aa3fd2021
Ryan237mba <afuhryan1@gmail.com>
2024-06-03 00:46:11 +0100

    RDS instance for mysql in a database subnet group, RYAN and LINUS work

--- vpc.tf
+++ vpc.tf
@@ -16,30 +16,43 @@ resource "aws_internet_gateway" "gw" {
 }
 
 resource "aws_subnet" "public_subnet" {
+  count=3
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.public_subnet_cidr
-
+  cidr_block = "${cidrsubnet(var.vpc_cidr,8, count.index)}"
+  map_public_ip_on_launch = "true"
   tags = {
-    Name = local.public_subnet_name
+    Name = "${local.public_subnet_name}_${count.index}"
   }
 }
+ 
 
 resource "aws_subnet" "private_subnet" {
+  count=3
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.private_subnet_cidr
-
+  cidr_block = "${cidrsubnet(var.vpc_cidr,6, count.index + 2)}"
   tags = {
-    Name = local.private_subnet_name
+    Name = "${local.private_subnet_name}_${count.index}"
   }
 }
 
+resource "aws_subnet" "database" {
+count = 3
+vpc_id = aws_vpc.vpc.id
+cidr_block = "${cidrsubnet(var.vpc_cidr,4, count.index + 4)}"
+tags = {
+  Name = "dbsub_${count.index}"
+}
+
+}
 resource "aws_route_table_association" "pub_rt" {
-  subnet_id      = aws_subnet.public_subnet.id
+  count=3
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "pri_rt" {
-  subnet_id      = aws_subnet.private_subnet.id
+   count=3
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
 
@@ -68,6 +81,18 @@ resource "aws_route_table" "private_rt" {
     Name = local.private_route_table
   }
 }
+resource "aws_route_table" "database" {
+vpc_id = aws_vpc.vpc.id
+tags = {
+  Name= "database-route-database"
+}
+}
+
+resource "aws_route_table_association" "database" {
+  count = 3
+  subnet_id = element(aws_subnet.database.*.id, count.index)
+  route_table_id = aws_route_table.database.id
+}
 
 resource "aws_eip" "lb" {
   domain = "vpc"
@@ -75,7 +100,7 @@ resource "aws_eip" "lb" {
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.lb.id
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id      = element(aws_subnet.public_subnet.*.id, 0)
 
   tags = {
     Name = local.ngw_name
@@ -83,3 +108,15 @@ resource "aws_nat_gateway" "nat" {
 
   depends_on = [aws_internet_gateway.gw]
 }
+
+
+
+resource "aws_db_subnet_group" "db_sb_gp" {
+  name       = "main"
+  subnet_ids = "${aws_subnet.database.*.id}"
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+
